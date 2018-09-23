55852c9bab6ffc42fde04cedbb1710efd5fcf7fe
se-yoshi <se.yoshi@gmail.com>
2018-09-24 08:23:06 +0900

    サンプルページの追加

---
+++ Notes.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notes.Web.Models;
+using System.Diagnostics;
+
+namespace Notes.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Notes.Web/Controllers/NotesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Notes.Web.Controllers
+{
+    public class NotesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
